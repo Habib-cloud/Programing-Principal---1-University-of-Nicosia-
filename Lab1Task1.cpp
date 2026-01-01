e87315e3c9ce7b266bd701fba7b1454ab5a28b65
Habib-cloud <147046754+Habib-cloud@users.noreply.github.com>
2026-01-01 22:34:51 +0200

    Add files via upload

---
+++ Lab1Task1.cpp
@@ -0,0 +1,22 @@
+/*
+Name: MD Habibur Rahman
+Date: 30 October 2025
+
+Question: General print of Name, Major, Hobbies in C++
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	// print name
+	cout << "\nMy name is: MD Habibur Rahman\n\n";
+
+		// print major
+		cout << "My major is: Computer Science\n\n";
+
+		// print Hobbies
+		cout << "My hobbies are: Watching movies, listenig to music, travelling and bike riding\n";
+
+	return 0;
+}
